418c766a809b5a92b4b99e4b52f4fe04cc64e88f
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2023-04-13 13:32:13 +0200

    Add files via upload

---
+++ tasks-model/src/main/java/com/curso/app/tasks/repository/UsuariosConListasDeTareasRepository.java
@@ -0,0 +1,9 @@
+package com.curso.app.tasks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.app.tasks.models.UsuarioConListasDeTareas;
+
+public interface UsuariosConListasDeTareasRepository extends JpaRepository<UsuarioConListasDeTareas, Long>{
+
+}
